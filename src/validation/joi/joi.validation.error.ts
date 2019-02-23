f6be36efc689b931b2f2c8b7e0c56a5af82b79d2
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-02-23 19:19:03 +0100

    fix: instanceof JoiValidationError

--- src/validation/joi/joi.validation.error.ts
+++ src/validation/joi/joi.validation.error.ts
@@ -9,5 +9,20 @@ export interface JoiValidationErrorData extends ErrorData {
 export class JoiValidationError extends AppError<JoiValidationErrorData> {
   constructor (message: string, data: JoiValidationErrorData) {
     super(message, data)
+
+    this.constructor = JoiValidationError
+    ;(this as any).__proto__ = JoiValidationError.prototype
+    Object.defineProperty(this, 'name', {
+      value: this.constructor.name,
+      configurable: true,
+    })
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor)
+    } else {
+      Object.defineProperty(this, 'stack', {
+        value: new Error().stack,
+      })
+    }
   }
 }
