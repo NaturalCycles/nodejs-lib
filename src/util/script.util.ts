ce4bfd68cc39f62650beda44006bc83230c0b652
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-11-05 17:33:08 +0100

    feat: runScript with uncaughtException logger and memoryInterval

--- src/util/script.util.ts
+++ src/util/script.util.ts
@@ -1,3 +1,12 @@
+import { mb } from '..'
+
+export interface RunScriptOptions {
+  /**
+   * If defined (milliseconds) - will do a setInterval with a timer that will log heapUsed.
+   */
+  memoryInterval?: number
+}
+
 /**
  * Use it in your top-level scripts like this:
  *
@@ -12,11 +21,34 @@
  * - No need to add `.then(() => process.exit()` (e.g to close DB connections)
  * - No need to add `.catch(err => { console.error(err); process.exit(1) })`
  */
-export function runScript(fn: (...args: any[]) => Promise<any>): void {
-  fn()
-    .then(() => process.exit(0))
-    .catch(err => {
+export function runScript(fn: (...args: any[]) => any, opt: RunScriptOptions = {}): void {
+  process.on('uncaughtException', err => {
+    console.error('uncaughtException', err)
+  })
+  process.on('unhandledRejection', err => {
+    console.error('unhandledRejection', err)
+  })
+
+  const { memoryInterval } = opt
+
+  if (memoryInterval) {
+    setInterval(logMemory, memoryInterval)
+  }
+
+  void (async () => {
+    try {
+      await fn()
+      setImmediate(() => process.exit(0))
+    } catch (err) {
       console.error(err)
       process.exit(1)
-    })
+    }
+  })()
+}
+
+function logMemory(): void {
+  const { heapUsed } = process.memoryUsage()
+  console.log({
+    heapUsed: mb(heapUsed),
+  })
 }
