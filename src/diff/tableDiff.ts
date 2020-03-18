b2a7392e6c64e1ba700300a4b474bc5182bec049
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-03-19 00:45:04 +0100

    feat: TableDiffOptions.aTitle, bTitle

--- src/diff/tableDiff.ts
+++ src/diff/tableDiff.ts
@@ -12,6 +12,18 @@ export interface TableDiffOptions {
    * If set - values of each field will be stringified and limited to maxFieldLen.
    */
   maxFieldLen?: number
+
+  /**
+   * Title of the A column
+   * @default `a`
+   */
+  aTitle?: string
+
+  /**
+   * Title of the B column
+   * @default `b`
+   */
+  bTitle?: string
 }
 
 /**
@@ -22,15 +34,15 @@ export interface TableDiffOptions {
  * Function is located in nodejs-lib (not js-lib), because it's planned to improve in the future and add e.g colors (via chalk).
  */
 export function tableDiff(a: object, b: object, opt: TableDiffOptions = {}): void {
-  const { maxFieldLen } = opt
+  const { maxFieldLen, aTitle = 'a', bTitle = 'b' } = opt
   const diff: StringMap<any> = {}
 
   if (a && b && a !== b) {
     new Set([...Object.keys(a), ...Object.keys(b)]).forEach(k => {
       if (a[k] !== b[k]) {
         diff[k] = {
-          a: maxFieldLen && a[k] ? _truncate(String(a[k]), maxFieldLen) : a[k],
-          b: maxFieldLen && b[k] ? _truncate(String(b[k]), maxFieldLen) : b[k],
+          [aTitle]: maxFieldLen && a[k] ? _truncate(String(a[k]), maxFieldLen) : a[k],
+          [bTitle]: maxFieldLen && b[k] ? _truncate(String(b[k]), maxFieldLen) : b[k],
         }
       }
     })
