52b320e1107a69aa6ee1af0614b85ccc0b086f31
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-03-21 20:36:53 +0100

    feat: export more colors

--- scripts/colors.script.ts
+++ scripts/colors.script.ts
@@ -8,12 +8,17 @@ import { chalk, runScript } from '../src'
 
 const s = 'Hello World! 1 2 3 4 5ms'
 
-const colors = ['white', 'grey', 'yellow', 'green']
+const colors = ['white', 'grey', 'yellow', 'green', 'red', 'blue', 'magenta', 'cyan']
+const modifiers = ['dim', null, 'bold', 'inverse']
 
 runScript(async () => {
   colors.forEach(color => {
-    console.log(chalk[color].dim(`${s} dim ${color}`))
-    console.log(chalk[color](`${s} ${color}`))
-    console.log(chalk[color].bold(`${s} bold ${color}`))
+    modifiers.forEach(mod => {
+      if (mod) {
+        console.log(chalk[color][mod](`${s} ${mod} ${color}`))
+      } else {
+        console.log(chalk[color](`${s} ${color}`))
+      }
+    })
   })
 })
