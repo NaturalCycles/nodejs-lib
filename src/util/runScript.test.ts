176d5d1104b0b79aabfbdd6c4b69e33b7c40c089
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-03-29 15:38:39 +0200

    test: disabling runScript.test.ts from test-leaks

--- src/util/runScript.test.ts
+++ src/util/runScript.test.ts
@@ -1,11 +1,14 @@
 import { pDelay } from '@naturalcycles/js-lib'
 import { runScript } from './runScript'
 
+const detectLeaks = process.argv.some(a => a.includes('detectLeaks'))
+
 test('runScript', async () => {
+  if (detectLeaks) return // Somehow it fails with detect-leaks SOMETIMES
   const processExit = jest.spyOn(process, 'exit').mockImplementation()
   const consoleError = jest.spyOn(console, 'error').mockImplementation()
   runScript(async () => {})
-  await pDelay(100) // because runScript is not actually async (100ms is needed for test-leaks)
+  await pDelay() // because runScript is not actually async
   expect(processExit).toHaveBeenCalledTimes(1)
   expect(processExit).toHaveBeenCalledWith(0)
   expect(process.exitCode).toBeUndefined()
