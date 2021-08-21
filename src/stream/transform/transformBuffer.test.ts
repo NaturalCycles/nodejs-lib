5ace8ebefa1f3ea0e75a9e4dbde038b4c6257f31
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-08-21 14:37:36 +0200

    chore: remove unused stream methods
    Might be a bit breaking, but easily replaceable

--- src/stream/transform/transformBuffer.test.ts
+++ src/stream/transform/transformBuffer.test.ts
@@ -1,12 +1,18 @@
 import { _range } from '@naturalcycles/js-lib'
-import { readableFromArray } from '../..'
-import { pipelineToArray } from '../pipeline/pipelineToArray'
+import { Readable } from 'stream'
+import { writablePushToArray, _pipeline } from '../..'
 import { transformBuffer } from './transformBuffer'
 
 test('transformBuffer', async () => {
   const data = _range(1, 6).map(n => ({ id: String(n) }))
-  const readable = readableFromArray(data)
 
-  const data2 = await pipelineToArray([readable, transformBuffer({ batchSize: 2 })])
+  const data2: any[] = []
+
+  await _pipeline([
+    Readable.from(data),
+    transformBuffer({ batchSize: 2 }),
+    writablePushToArray(data2),
+  ])
+
   expect(data2).toEqual([[{ id: '1' }, { id: '2' }], [{ id: '3' }, { id: '4' }], [{ id: '5' }]])
 })
