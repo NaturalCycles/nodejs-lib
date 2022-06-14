57fd2face2ad83a9a4cb49cb122deec3226112a7
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-06-14 23:41:31 +0200

    chore: deps

--- scripts/colors.script.ts
+++ scripts/colors.script.ts
@@ -4,13 +4,23 @@ yarn tsn colors.script.ts
 
  */
 
+import { Color, Modifiers } from 'chalk'
 import { chalk } from '../src/colors'
 import { runScript } from '../src/script'
 
 const s = 'Hello World! 1 2 3 4 5ms'
 
-const colors = ['white', 'grey', 'yellow', 'green', 'red', 'blue', 'magenta', 'cyan']
-const modifiers = ['dim', null, 'bold', 'inverse']
+const colors: typeof Color[] = [
+  'white',
+  'grey',
+  'yellow',
+  'green',
+  'red',
+  'blue',
+  'magenta',
+  'cyan',
+]
+const modifiers: typeof Modifiers[] = ['dim', null as any, 'bold', 'inverse']
 
 runScript(async () => {
   colors.forEach(color => {
