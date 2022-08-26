ac50efc30626c99e8ad10d7b200e4221b9247fb8
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-08-26 02:01:42 +0200

    feat: delSync

--- src/fs/del.ts
+++ src/fs/del.ts
@@ -110,6 +110,78 @@ export async function del(_opt: DelOptions | DelSingleOption): Promise<void> {
   }
 }
 
+export function delSync(_opt: DelOptions | DelSingleOption): void {
+  const started = Date.now()
+
+  // Convert DelSingleOption to DelOptions
+  if (typeof _opt === 'string') {
+    _opt = {
+      patterns: [_opt],
+    }
+  }
+
+  const opt = {
+    ...DEF_OPT,
+    ..._opt,
+  }
+  const { patterns, verbose, silent, debug, dry } = opt
+
+  if (debug) {
+    console.log(opt)
+  }
+
+  // 1. glob only files, expand dirs, delete
+
+  const filenames = globby.sync(patterns, {
+    dot: true,
+    expandDirectories: true,
+    onlyFiles: true,
+  })
+
+  if (verbose || debug || dry) {
+    console.log(`Will delete ${yellow(filenames.length)} files:`, filenames)
+  }
+
+  if (dry) return
+
+  filenames.forEach(filepath => fs.removeSync(filepath))
+
+  // 2. glob only dirs, expand, delete only empty!
+  let dirnames = globby.sync(patterns, {
+    dot: true,
+    expandDirectories: true,
+    onlyDirectories: true,
+  })
+
+  // Add original patterns (if any of them are dirs)
+  dirnames = dirnames.concat(
+    patterns.filter(p => fs.pathExistsSync(p) && fs.lstatSync(p).isDirectory()),
+  )
+
+  const dirnamesSorted = dirnames.sort().reverse()
+
+  // console.log({ dirnamesSorted })
+
+  const deletedDirs: string[] = []
+  for (const dirpath of dirnamesSorted) {
+    if (isEmptyDirSync(dirpath)) {
+      // console.log(`empty dir: ${dirpath}`)
+      fs.removeSync(dirpath)
+      deletedDirs.push(dirpath)
+    }
+  }
+
+  if (verbose || debug) console.log({ deletedDirs })
+
+  if (!silent) {
+    console.log(
+      `del deleted ${yellow(filenames.length)} files and ${yellow(
+        deletedDirs.length,
+      )} dirs ${dimGrey(_since(started))}`,
+    )
+  }
+}
+
 // Improved algorithm:
 // 1. glob only files, expand dirs, delete
 // 2. glob only dirs, expand, delete only empty!
@@ -117,4 +189,8 @@ export async function del(_opt: DelOptions | DelSingleOption): Promise<void> {
 
 async function isEmptyDir(dir: string): Promise<boolean> {
   return (await fs.readdir(dir)).length === 0
+}
+
+function isEmptyDirSync(dir: string): boolean {
+  return fs.readdirSync(dir).length === 0
 }
--- src/fs/index.ts
+++ src/fs/index.ts
@@ -1,7 +1,7 @@
-import { del, DelOptions } from './del'
+import { del, delSync, DelOptions } from './del'
 import { json2env, objectToShellExport } from './json2env'
 import { kpy, KpyOptions, kpySync } from './kpy'
 
 export type { KpyOptions, DelOptions }
 
-export { kpy, kpySync, del, objectToShellExport, json2env }
+export { kpy, kpySync, del, delSync, objectToShellExport, json2env }
