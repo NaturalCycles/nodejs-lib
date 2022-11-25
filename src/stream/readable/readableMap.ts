9f1b3ee09339fcd3abf631a209ee09f04ae79a69
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-11-25 14:55:13 +0100

    fix: readableMap to propagate errors better

--- src/stream/readable/readableMap.ts
+++ src/stream/readable/readableMap.ts
@@ -8,24 +8,27 @@ export function readableMap<IN, OUT>(
 ): ReadableTyped<OUT> {
   let i = -1
 
-  // todo: check if we need to handle errors somehow specifically
-  return readable.pipe(
-    new Transform({
-      objectMode: true,
-      async transform(chunk, _enc, cb) {
-        try {
-          const r = await mapper(chunk, ++i)
-          if (r === SKIP) {
-            cb()
-          } else {
-            // _assert(r !== END, `readableMap END not supported`)
-            cb(null, r)
+  const stream: ReadableTyped<OUT> = readable
+    .on('error', err => stream.emit('error', err))
+    .pipe(
+      new Transform({
+        objectMode: true,
+        async transform(chunk, _enc, cb) {
+          try {
+            const r = await mapper(chunk, ++i)
+            if (r === SKIP) {
+              cb()
+            } else {
+              // _assert(r !== END, `readableMap END not supported`)
+              cb(null, r)
+            }
+          } catch (err) {
+            console.error(err)
+            cb(err as Error)
           }
-        } catch (err) {
-          console.error(err)
-          cb(err as Error)
-        }
-      },
-    }),
-  )
+        },
+      }),
+    )
+
+  return stream
 }
