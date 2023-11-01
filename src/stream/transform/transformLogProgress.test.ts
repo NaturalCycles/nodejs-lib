76875b06f7c64e57b82d437afffc86d3eae43923
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2023-11-01 10:11:12 +0100

    chore: deps

--- src/stream/transform/transformLogProgress.test.ts
+++ src/stream/transform/transformLogProgress.test.ts
@@ -1,11 +1,24 @@
 import { Readable } from 'node:stream'
-import { AsyncSequence, END, pDelay } from '@naturalcycles/js-lib'
+import { pDelay } from '@naturalcycles/js-lib'
 import { _pipeline, writableVoid } from '../..'
 import { transformLogProgress } from './transformLogProgress'
 
+// todo: AsyncIterable2 (or Iterable2.mapAsync) should be implemented in js-lib
+async function* rangeItAsync(
+  fromIncl: number,
+  toExcl: number,
+  delay: number,
+): AsyncIterable<number> {
+  for (let i = fromIncl; i < toExcl; i++) {
+    await pDelay(delay)
+    yield i
+  }
+}
+
 test('transformLogProgress', async () => {
   // const readable = readableFromArray(_range(0, 11), i => pDelay(10, i))
-  const readable = Readable.from(AsyncSequence.create(1, i => (i === 10 ? END : pDelay(10, i + 1))))
+  // const readable = Readable.from(AsyncSequence.create(1, i => (i === 10 ? END : pDelay(10, i + 1))))
+  const readable = Readable.from(rangeItAsync(1, 11, 10))
 
   await _pipeline([
     readable,
