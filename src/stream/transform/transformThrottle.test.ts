1c2537e34353e32902bea63e18975c7a4c64926b
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2024-07-16 17:23:30 +0200

    feat: transformThrottle

---
+++ src/stream/transform/transformThrottle.test.ts
@@ -0,0 +1,25 @@
+import { Readable } from 'node:stream'
+import { _range, ObjectWithId } from '@naturalcycles/js-lib'
+import { _pipeline } from '../pipeline/pipeline'
+import { writableVoid } from '../writable/writableVoid'
+import { transformTap } from './transformTap'
+import { transformThrottle } from './transformThrottle'
+
+test('transformThrottle', async () => {
+  await _pipeline([
+    // super-fast producer
+    Readable.from(_range(1, 11).map(id => ({ id: String(id) }))),
+    // transformTap(obj => {
+    //   console.log('pre', obj)
+    // }),
+    transformThrottle<ObjectWithId>({
+      interval: 1,
+      throughput: 3,
+      // debug: true,
+    }),
+    transformTap(obj => {
+      console.log('post', obj)
+    }),
+    writableVoid(),
+  ])
+}, 20_000)
---
+++ src/stream/transform/transformThrottle.ts
@@ -0,0 +1,104 @@
+import { Transform } from 'node:stream'
+import {
+  _ms,
+  DeferredPromise,
+  localTime,
+  NumberOfMilliseconds,
+  NumberOfSeconds,
+  pDefer,
+  PositiveInteger,
+} from '@naturalcycles/js-lib'
+import { TransformTyped } from '../stream.model'
+
+export interface TransformThrottleOptions {
+  /**
+   * How many items to allow per `interval` of seconds.
+   */
+  throughput: PositiveInteger
+
+  /**
+   * How long is the interval (in seconds) where number of items should not exceed `throughput`.
+   */
+  interval: NumberOfSeconds
+
+  debug?: boolean
+}
+
+/**
+ * Allows to throttle the throughput of the stream.
+ * For example, when you have an API with rate limit of 5000 requests per minute,
+ * `transformThrottle` can help you utilize it most efficiently.
+ * You can define it as:
+ *
+ * _pipeline([
+ *   // ...
+ *   transformThrottle({
+ *     throughput: 5000,
+ *     interval: 60,
+ *   }),
+ *   // ...
+ * ])
+ *
+ * @experimental
+ */
+export function transformThrottle<T>(opt: TransformThrottleOptions): TransformTyped<T, T> {
+  const { throughput, interval, debug } = opt
+
+  let count = 0
+  let start: NumberOfMilliseconds
+  let paused: DeferredPromise | undefined
+  let timeout: NodeJS.Timeout | undefined
+
+  return new Transform({
+    objectMode: true,
+    async transform(item: T, _, cb) {
+      // console.log('incoming', item, { paused: !!paused, count })
+      if (!start) {
+        start = Date.now()
+        timeout = setTimeout(() => onInterval(this), interval * 1000)
+        if (debug) {
+          console.log(`${localTime.now().toPretty()} transformThrottle started with`, {
+            throughput,
+            interval,
+            rps: Math.round(throughput / interval),
+          })
+        }
+      }
+
+      if (paused) {
+        // console.log('awaiting pause', {item, count})
+        await paused
+      }
+
+      if (++count >= throughput) {
+        // console.log('pausing now after', {item, count})
+        paused = pDefer()
+        if (debug) {
+          console.log(
+            `${localTime.now().toPretty()} transformThrottle activated: ${count} items passed in ${_ms(interval * 1000)}, will pause for ${_ms(interval * 1000 - (Date.now() - start))}`,
+          )
+        }
+      }
+
+      cb(null, item) // pass the item through
+    },
+    final(cb) {
+      clearTimeout(timeout)
+      cb()
+    },
+  })
+
+  function onInterval(transform: Transform): void {
+    if (!paused) return
+
+    if (debug) {
+      console.log(`${localTime.now().toPretty()} transformThrottle resumed`)
+    }
+
+    count = 0
+    start = Date.now()
+    timeout = setTimeout(() => onInterval(transform), interval * 1000)
+    paused.resolve()
+    paused = undefined
+  }
+}
